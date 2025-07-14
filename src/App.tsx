a52b837067d7e7849bf28c84bbcdbfe3be3415a9
Youssef Ben Salem <youssefbselem@gmail.com>
2025-07-14 01:34:52 +0100

    Add ScrollToTop component for improved navigation experience
    Integrate the ScrollToTop component into the App to enhance user navigation by automatically scrolling to the top of the page when navigating between routes. This addition aims to improve overall user experience and accessibility within the application.

--- src/App.tsx
+++ src/App.tsx
@@ -8,6 +8,7 @@ import Master from "./pages/Master";
 import Concours from "./pages/Concours";
 import Alternance from "./pages/Alternance";
 import NotFound from "./pages/NotFound";
+import ScrollToTop from "./components/ScrollToTop";
 
 const queryClient = new QueryClient();
 
@@ -17,6 +18,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/master" element={<Master />} />
